ae56152aae5ab51794969e8ef6849b9c448cd6df
krisztian.radics21 <krisztian.radics21@stud.uni-obuda.hu>
2022-10-24 22:35:53 +0200

    Competitor class added to Models

---
+++ TB1IGK_HFT_2022231.Models/Competitor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TB1IGK_HFT_2022231.Models
+{
+    public class Competitor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [StringLength(40)]
+        public string Name { get; set; }
+        public int Age { get; set; }
+        [ForeignKey(nameof(Competition))]
+        public int CompetitonID { get; set; }
+        [ForeignKey(nameof(Category))]
+        public int CategoryID { get; set; }
+        [StringLength(3)]
+        public string Nation { get; set; }
+
+        public Competitor()
+        {
+        }
+
+        public Competitor(int id, string name, int age, int competitionID, int categoryID, string nation)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Age = age;
+            this.CompetitonID = competitionID;
+            this.CategoryID = categoryID;
+            this.Nation = nation;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}\tName: {Name}\tAge: {Age}\tCompetition ID: {CompetitonID}\tCategory ID: {CategoryID}\tNation: {Nation}";
+        }
+    }
+}
