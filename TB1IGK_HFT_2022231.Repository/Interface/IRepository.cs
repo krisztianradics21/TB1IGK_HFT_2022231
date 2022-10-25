0f046dc70c7e625ccd4fd866cba4b7f4610df618
krisztian.radics21 <krisztian.radics21@stud.uni-obuda.hu>
2022-10-25 20:13:49 +0200

    IRepository interface created and filled with CRUD methods

---
+++ TB1IGK_HFT_2022231.Repository/Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TB1IGK_HFT_2022231.Repository.Interface
+{
+    public interface IRepository<T> where T : class
+    {
+        //CRUD methods
+        T GetOne(int id);
+        IQueryable<T> GetAll();
+        void Create(T input);
+        void Delete(int id);
+        void Update(T input);
+    }
+}
