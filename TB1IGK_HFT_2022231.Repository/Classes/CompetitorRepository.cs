249bb790ab6f55be6e48447e161f19ed6a642a45
krisztian.radics21 <krisztian.radics21@stud.uni-obuda.hu>
2022-10-27 19:42:14 +0200

    CompetitorRepository created

---
+++ TB1IGK_HFT_2022231.Repository/Classes/CompetitorRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TB1IGK_HFT_2022231.Models;
+using TB1IGK_HFT_2022231.Repository.Interface;
+
+namespace TB1IGK_HFT_2022231.Repository
+{
+    class CompetitorRepository : Repository<Competitor>, IRepository<Competitor>
+    {
+        public CompetitorRepository(DbContext ctx) : base(ctx)
+        {
+
+        }
+        public override void Create(Competitor input)
+        {
+            this._Ctx.Add(input);
+            _Ctx.SaveChanges();
+        }
+
+        public override void Delete(int input)
+        {
+            Competitor c = GetOne(input);
+            this._Ctx.Remove(c);
+            _Ctx.SaveChanges();
+        }
+
+        public override Competitor GetOne(int id)
+        {
+            return GetAll().FirstOrDefault(x => x.ID.Equals(id));
+        }
+
+        public override void Update(Competitor input)
+        {
+            Competitor competitor = GetOne(input.ID);
+            competitor.CompetitonID = input.CompetitonID;
+            competitor.Name = input.Name;
+            competitor.CategoryID = input.CategoryID;
+            competitor.Nation = input.Nation;
+            competitor.Age = input.Age;
+            _Ctx.SaveChanges();
+        }
+    }
+}
