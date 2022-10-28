dac9657a338a6b66268ee1335009a4a4a52a8b6b
krisztian.radics21 <krisztian.radics21@stud.uni-obuda.hu>
2022-10-28 11:56:50 +0200

    ILogic interface filled with crud methods

---
+++ TB1IGK_HFT_2022231.Logic/ILogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TB1IGK_HFT_2022231.Logic
+{
+    public interface ILogic<T> 
+    {
+        T GetOne(int id);
+        IQueryable<T> GetAll();
+        void Create(T input);
+        void Delete(int id);
+        void Update(T input);
+    }
+}
