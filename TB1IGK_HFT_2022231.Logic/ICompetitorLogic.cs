e4af8672ad77952a2f1552f5ab24f08318caa531
krisztian.radics21 <krisztian.radics21@stud.uni-obuda.hu>
2022-10-29 12:13:38 +0200

    ICompetitorLogic created

---
+++ TB1IGK_HFT_2022231.Logic/ICompetitorLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TB1IGK_HFT_2022231.Models;
+
+namespace TB1IGK_HFT_2022231.Logic
+{
+    interface ICompetitorLogic : ILogic<Competitor>
+    {
+        double AVGAge();
+        IEnumerable<object> CompetitorWithAllRelevantData();
+    }
+}
