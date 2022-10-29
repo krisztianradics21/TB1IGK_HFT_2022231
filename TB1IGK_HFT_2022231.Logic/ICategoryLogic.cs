20a21f1904abbdf2b53e2d4223d8a1453cd464fd
krisztian.radics21 <krisztian.radics21@stud.uni-obuda.hu>
2022-10-29 12:12:16 +0200

    ICategoryLogic created

---
+++ TB1IGK_HFT_2022231.Logic/ICategoryLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TB1IGK_HFT_2022231.Models;
+
+namespace TB1IGK_HFT_2022231.Logic
+{
+    public interface ICategoryLogic : ILogic<Category>
+    {
+        IEnumerable<KeyValuePair<string, string>> CompetitorsByBoatCategory();
+    }
+}
