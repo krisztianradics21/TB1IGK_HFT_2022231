5b2bb9fef915855fc9b548388452cafdd6fdcbb3
krisztian.radics21 <krisztian.radics21@stud.uni-obuda.hu>
2022-11-20 15:02:37 +0100

    CategoryController class added

---
+++ TB1IGK_HFT_2022231.Endpoint/Controllers/CategoryController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TB1IGK_HFT_2022231.Logic;
+using TB1IGK_HFT_2022231.Models;
+
+namespace TB1IGK_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        ICategoryLogic categoryLogic;
+
+        public CategoryController(ICategoryLogic categoryLogic)
+        {
+            this.categoryLogic = categoryLogic;
+        }
+
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return categoryLogic.GetAll();
+        }
+
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        public Category Get(int id)
+        {
+            return categoryLogic.GetOne(id);
+        }
+
+        // POST api/<CategoryController>
+        [HttpPost]
+        public void Post([FromBody] Category value)
+        {
+            categoryLogic.Create(value);
+        }
+
+        // PUT api/<CategoryController>/5
+        [HttpPut]
+        public void Put([FromBody] Category value)
+        {
+            categoryLogic.Update(value);
+        }
+
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            categoryLogic.Delete(id);
+        }
+    }
+}
