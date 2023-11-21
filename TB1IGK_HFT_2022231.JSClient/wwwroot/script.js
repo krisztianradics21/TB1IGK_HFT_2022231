ef8d0c7ab1eb20964657c43bd130b70aec6f0f7e
krisztian.radics21 <krisztian.radics21@stud.uni-obuda.hu>
2023-11-21 19:51:01 +0100

    Competitor JS GET

--- TB1IGK_HFT_2022231.JSClient/wwwroot/script.js
+++ TB1IGK_HFT_2022231.JSClient/wwwroot/script.js
@@ -1,3 +1,20 @@
-﻿fetch('http://localhost:55475/competitor')
+﻿let competitors = [];
+
+fetch('http://localhost:55475/competitor')
     .then(x => x.json())
-    .then(y => console.log(y));
+    .then(y => {
+        competitors = y;
+        console.log(competitors);
+        display();
+    });
+function display() {
+    competitors.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.id + "</td><td>"
+            + t.name + "</td><td>"
+            + t.age + "</td><td>"
+            + t.competitonID + "</td><td>"
+            + t.categoryID + "</td><td>"
+        + t.nation + "</td></tr>"
+    });
+}
